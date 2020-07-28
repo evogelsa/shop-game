68d1b29936988615b4e6efb176a4eb800f103fbb
Ethan Vogelsang <ethanvogelsang@gmail.com>
2020-07-28 01:22:17 -0400

    add initial tests for pedestrian movement

---
+++ Shop Game/Assets/Scripts/PedSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedSpawner : MonoBehaviour
+{
+    public GameObject[] peds;
+    private GameObject ped;
+
+    private float maxTimeBetweenSpawn = 2f;
+    private float minTimeBetweenSpawn = 0.2f;
+    private float timeBetweenSpawn = 0f;
+
+    void FixedUpdate()
+    {
+        if (timeBetweenSpawn <= 0)
+        {
+            ped = peds[Random.Range(0, peds.Length)];
+            Instantiate(ped, transform.position, Quaternion.identity);
+            timeBetweenSpawn = Random.Range(minTimeBetweenSpawn, maxTimeBetweenSpawn);
+        }
+        else
+        {
+            timeBetweenSpawn -= Time.deltaTime;
+        }
+    }
+}
---
+++ Shop Game/Assets/Scripts/Pedestrian.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedestrian : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private float maxSpeed = 3f;
+    private float minSpeed = 2f;
+    private float sidewalkWidth = 2f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        Vector3 startPos = transform.position;
+        startPos.y -= Random.value * sidewalkWidth;
+        transform.position = startPos;
+        rb.velocity = Vector2.left * (Random.value*(maxSpeed-minSpeed)+minSpeed);
+
+        sprite.sortingOrder = (int) (-transform.position.y/sidewalkWidth * 100);
+    }
+
+    void Update()
+    {
+        if (transform.position.x < -15)
+            Destroy(gameObject);
+    }
+
+    void FixedUpdate() {
+    }
+}
