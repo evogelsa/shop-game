a1d9aa7f51c6b5f9eaafcfba662acede850bb9d0
Ethan Vogelsang <ethanvogelsang@gmail.com>
2020-08-07 20:46:10 -0400

    remove 3d assets and level

--- Shop Game/Assets/Scripts/Pedestrian.cs
+++ Shop Game/Assets/Scripts/Pedestrian.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Pedestrian : MonoBehaviour {
 
     private Rigidbody2D rb;
-    private SpriteRenderer sprite;
+    private SpriteRenderer[] sprites;
     private float maxSpeed = 3f;
     private float minSpeed = 2f;
     private float sidewalkWidth = 2f;
@@ -13,13 +13,17 @@ public class Pedestrian : MonoBehaviour {
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
+        sprites = GetComponentsInChildren<SpriteRenderer>();
 
         Vector3 startPos = transform.position;
         startPos.y -= Random.value * sidewalkWidth;
         transform.position = startPos;
         rb.velocity = Vector2.left * (Random.value*(maxSpeed-minSpeed)+minSpeed);
 
-        sprite.sortingOrder = (int) (-transform.position.y/sidewalkWidth * 100);
+        if (sprites != null)
+        {
+            foreach (SpriteRenderer sprite in sprites)
+                sprite.sortingOrder += (int) (-transform.position.y/sidewalkWidth * 100);
+        }
     }
 }
